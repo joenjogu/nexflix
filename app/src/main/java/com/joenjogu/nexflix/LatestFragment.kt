9ab9e9a77186cb212474fdc4b16a3486c65e970b
Joe Njogu <joenjogu97@gmail.com>
2020-12-09 10:00:04 +0300

    added movie layout for both fragments with data binding

--- app/src/main/java/com/joenjogu/nexflix/LatestFragment.kt
+++ app/src/main/java/com/joenjogu/nexflix/LatestFragment.kt
@@ -1,7 +1,18 @@
 package com.joenjogu.nexflix
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
 class LatestFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
 
 }
