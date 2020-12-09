dc1a9585cd10c1c679d56c7e15d4d29883fa6d5b
Joe Njogu <joenjogu97@gmail.com>
2020-12-09 08:28:51 +0300

    set up tab layout with viewpager2

---
+++ app/src/main/java/com/joenjogu/nexflix/LatestFragment.kt
@@ -0,0 +1,7 @@
+package com.joenjogu.nexflix
+
+import androidx.fragment.app.Fragment
+
+class LatestFragment : Fragment() {
+
+}
--- app/src/main/java/com/joenjogu/nexflix/MainActivity.kt
+++ app/src/main/java/com/joenjogu/nexflix/MainActivity.kt
@@ -2,10 +2,28 @@ package com.joenjogu.nexflix
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tabLayout = findViewById<TabLayout>(R.id.tab_layout)
+        val viewPager = findViewById<ViewPager2>(R.id.viewpager)
+
+        val fragmentAdapter = FragmentAdapter(supportFragmentManager, lifecycle)
+        viewPager.adapter = fragmentAdapter
+        viewPager.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+
+        TabLayoutMediator(tabLayout, viewPager){ tab, position ->
+            when (position) {
+                0 -> tab.text = getString(R.string.popular)
+                1 -> tab.text = getString(R.string.latest)
+            }
+            viewPager.setCurrentItem(0, true)
+        }.attach()
     }
 }
