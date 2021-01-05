34c7704d6b9cc0b2e4c46c7818297218e66b7391
Joe Njogu <joenjogu97@gmail.com>
2021-01-05 23:13:02 +0300

    movie detail fragment recyclerview ui

---
+++ app/src/main/java/com/joenjogu/nexflix/ProductionCompany.kt
@@ -0,0 +1,8 @@
+package com.joenjogu.nexflix
+
+data class ProductionCompany(
+    val id: Int,
+    val logo_path: String,
+    val name: String,
+    val origin_country: String
+)
--- app/src/main/java/com/joenjogu/nexflix/ui/MovieDetailFragment.kt
+++ app/src/main/java/com/joenjogu/nexflix/ui/MovieDetailFragment.kt
@@ -5,13 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.joenjogu.nexflix.R
+import com.joenjogu.nexflix.databinding.FragmentMovieDetailBinding
+import com.joenjogu.nexflix.databinding.MovieDetailRecyclerviewBinding
 
 class MovieDetailFragment : Fragment() {
+    private lateinit var detailBinding: FragmentMovieDetailBinding
+    private lateinit var movieBinding: MovieDetailRecyclerviewBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_movie_detail, container, false)
+        detailBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_movie_detail, container, false)
+
+//        impl movieBinding adapter
+
+        return detailBinding.root
     }
 }
