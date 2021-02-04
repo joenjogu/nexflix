bc1006f55ec33b5c073f248cb1e84bf9e7f19f9b
Joe Njogu <joenjogu97@gmail.com>
2021-02-04 22:54:37 +0300

    first test is MovieTest.kt

---
+++ app/src/test/java/com/joenjogu/nexflix/models/MovieTest.kt
@@ -0,0 +1,16 @@
+package com.joenjogu.nexflix.models
+
+import com.joenjogu.nexflix.utils.Category
+import org.junit.Assert.*
+import org.junit.Test
+
+class MovieTest {
+    private lateinit var movie: Movie
+
+    @Test
+    fun test_default_value() {
+        val movie = Movie(1, "https:// url", "Tenet", "time", 2.4, "2020", Category.TopRated)
+
+        assertEquals(0, movie.recommendedId)
+    }
+}
