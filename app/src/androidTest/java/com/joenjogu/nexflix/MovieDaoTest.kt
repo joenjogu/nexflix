c7195ad5d28ee852943084e7bff86f21d57c136d
Joe Njogu <joenjogu97@gmail.com>
2021-02-05 23:11:57 +0300

    test done for MovieDao, added test dependency

---
+++ app/src/androidTest/java/com/joenjogu/nexflix/MovieDaoTest.kt
@@ -0,0 +1,62 @@
+package com.joenjogu.nexflix
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.room.Room
+import androidx.test.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.joenjogu.nexflix.data.MovieDao
+import com.joenjogu.nexflix.data.MovieDatabase
+import com.joenjogu.nexflix.models.Movie
+import com.joenjogu.nexflix.utilities.getValue
+import com.joenjogu.nexflix.utils.Category
+import kotlinx.coroutines.runBlocking
+import org.hamcrest.Matchers.equalTo
+import org.junit.After
+import org.junit.Assert.assertThat
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class MovieDaoTest {
+
+    private lateinit var database: MovieDatabase
+    private lateinit var dao: MovieDao
+
+    private val movie1 = Movie(1, "https:// url", "Tenet", "time", 2.4, "2020", Category.TopRated)
+    private val movie2 = Movie(2, "https:// url", "Joker", "jokes", 5.2, "2020", Category.TopRated)
+    private val movie3 = Movie(3, "https:// url", "Crush", "banter", 8.9, "2020", Category.Trending)
+
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @Before
+    fun createDb() = runBlocking {
+        val context = InstrumentationRegistry.getInstrumentation().targetContext
+        database = Room.inMemoryDatabaseBuilder(context, MovieDatabase::class.java).build()
+        dao = database.movieDao
+
+        dao.insertAllMovies(listOf(movie1, movie2, movie3))
+    }
+
+    @After
+    fun closeDb() {
+        database.close()
+    }
+
+    @Test
+    fun testGetAllMovies() {
+        val movieList = getValue(dao.getAllMovies(Category.TopRated))
+        assertThat(movieList.size, equalTo(2))
+    }
+
+    @Test
+    fun testGetMovieById() {
+        val id = 2
+        runBlocking {
+            val value = dao.getMovieById(id)
+            assertThat(value.title, equalTo("Joker"))
+        }
+    }
+}
---
+++ app/src/androidTest/java/com/joenjogu/nexflix/utilities/LiveDataTestUtil.kt
@@ -0,0 +1,19 @@
+@file:Suppress("UNCHECKED_CAST")
+
+package com.joenjogu.nexflix.utilities
+
+import androidx.lifecycle.LiveData
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.TimeUnit
+
+fun <T> getValue(liveData: LiveData<T>): T {
+    val data = arrayOfNulls<Any>(1)
+    val latch =CountDownLatch(1)
+    liveData.observeForever{
+        data[0] = it
+        latch.countDown()
+    }
+    latch.await(2, TimeUnit.SECONDS)
+
+    return data[0] as T
+}
