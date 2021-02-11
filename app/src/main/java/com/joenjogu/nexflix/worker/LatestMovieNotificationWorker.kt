68f282ec42f0e71731ed21b4a0a7e59c15d7ca90
Joe Njogu <joenjogu97@gmail.com>
2021-02-11 19:02:52 +0300

    notification worker with api call and result handling

--- app/src/main/java/com/joenjogu/nexflix/worker/LatestMovieNotificationWorker.kt
+++ app/src/main/java/com/joenjogu/nexflix/worker/LatestMovieNotificationWorker.kt
@@ -1,9 +1,12 @@
 package com.joenjogu.nexflix.worker
 
+import android.accounts.NetworkErrorException
 import android.content.Context
+import android.util.Log
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import com.joenjogu.nexflix.data.MoviesApiService
+import com.joenjogu.nexflix.utils.toTrendingDomain
 
 class LatestMovieNotificationWorker(
     context: Context,
@@ -12,6 +15,27 @@ class LatestMovieNotificationWorker(
 ) : CoroutineWorker(context, workerParams){
 
     override suspend fun doWork(): Result {
+        try {
+            val response = apiService.getTrendingMovies()
+            if (response.isSuccessful) {
+                val moviesJson = response.body()
+                if (moviesJson != null) {
+                    val latestMovie = moviesJson.trendingResults[0].toTrendingDomain()
 
+                    // Add notification handling
+                    return Result.success()
+                }
+            } else {
+                val error = response.code().toString() + response.errorBody()
+                Log.d(TAG, "doWork: $error")
+            }
+        } catch (exception : NetworkErrorException) {
+            Log.d(TAG, "doWork: $exception")
+        }
+        return Result.failure()
+    }
+
+    companion object {
+        val TAG: String = LatestMovieNotificationWorker::class.java.name
     }
 }
