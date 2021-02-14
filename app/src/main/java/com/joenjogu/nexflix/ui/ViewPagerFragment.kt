37ef0e8d5b2384d61daee89fc9e401b9c1607d4e
Joe Njogu <joenjogu97@gmail.com>
2021-02-14 20:26:55 +0300

    app bar fixed for settings menu

--- app/src/main/java/com/joenjogu/nexflix/ui/ViewPagerFragment.kt
+++ app/src/main/java/com/joenjogu/nexflix/ui/ViewPagerFragment.kt
@@ -1,9 +1,14 @@
 package com.joenjogu.nexflix.ui
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
@@ -14,10 +19,12 @@ import com.joenjogu.nexflix.adapters.FragmentAdapter
 class ViewPagerFragment : Fragment() {
 
     companion object {
-        val TAG = ViewPagerFragment::class.java.name
+        val TAG: String = ViewPagerFragment::class.java.name
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val toolbar = view?.findViewById<Toolbar>(R.id.menu_app_bar)
+        (activity as AppCompatActivity).setSupportActionBar(toolbar)
 
         return inflater.inflate(R.layout.fragment_viewpager, container, false)
     }
@@ -25,7 +32,7 @@ class ViewPagerFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val viewPager = view.findViewById<ViewPager2>(R.id.viewpager)
         val tabLayout = view.findViewById<TabLayout>(R.id.tab_layout)
-        val fragmentManager = parentFragmentManager
+
 
         val fragmentAdapter = FragmentAdapter(childFragmentManager, lifecycle)
         viewPager.adapter = fragmentAdapter
@@ -42,20 +49,20 @@ class ViewPagerFragment : Fragment() {
 
     }
 
-//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-//        when (item.itemId) {
-//            R.id.menu_search -> {
-//                // search function
-//                return true
-//            }
-//            R.id.menu_settings -> {
-//                Log.d(TAG, "onOptionsItemSelected: Settings Button Clicked")
-//                val intent = Intent(context, SettingsActivity::class.java)
-//                startActivity(intent)
-//                return true
-//            }
-//            else -> super.onOptionsItemSelected(item)
-//        }
-//        return false
-//    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu_search -> {
+                // search function
+                return true
+            }
+            R.id.menu_settings -> {
+                Log.d(TAG, "onOptionsItemSelected: Settings Button Clicked")
+                val intent = Intent(context, SettingsActivity::class.java)
+                startActivity(intent)
+                return true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+        return false
+    }
 }
