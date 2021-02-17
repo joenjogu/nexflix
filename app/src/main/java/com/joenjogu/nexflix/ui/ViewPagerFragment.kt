105830c41252459f6a055d4c3aab0a886945cc29
Joe Njogu <joenjogu97@gmail.com>
2021-02-17 19:01:27 +0300

    ViewPagerFragment.kt appbar menu inflater

--- app/src/main/java/com/joenjogu/nexflix/ui/ViewPagerFragment.kt
+++ app/src/main/java/com/joenjogu/nexflix/ui/ViewPagerFragment.kt
@@ -3,10 +3,7 @@ package com.joenjogu.nexflix.ui
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.MenuItem
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
@@ -49,6 +46,11 @@ class ViewPagerFragment : Fragment() {
         }.attach()
 
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.top_app_bar, menu)
+        super.onCreateOptionsMenu(menu, inflater)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
