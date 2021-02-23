d18012e8e9bcce60fa4b440441b264bbbc6392c2
Joe Njogu <joenjogu97@gmail.com>
2021-02-23 19:04:23 +0300

    refactored to api response classes

--- app/src/main/java/com/joenjogu/nexflix/data/MovieApiService.kt
+++ app/src/main/java/com/joenjogu/nexflix/data/MovieApiService.kt
@@ -1,28 +1,27 @@
 package com.joenjogu.nexflix.data
 
 import com.joenjogu.nexflix.BuildConfig
-import com.joenjogu.nexflix.models.SearchResponse
 import com.joenjogu.nexflix.models.MovieResult
+import com.joenjogu.nexflix.models.SearchResponse
 import com.joenjogu.nexflix.models.TrendingResponse
-import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface MoviesApiService {
     @GET("movie/{movie_id}")
-    suspend fun getMovie(@Path("movie_id") movieId: Int, @Query("api_key") apiKey: String = BuildConfig.API_KEY): Response<MovieResult>
+    suspend fun getMovie(@Path("movie_id") movieId: Int, @Query("api_key") apiKey: String = BuildConfig.API_KEY): MovieResult
 
     @GET("movie/top_rated")
-    suspend fun getTopMovies(@Query("api_key") apiKey: String = BuildConfig.API_KEY): Response<SearchResponse>
+    suspend fun getTopMovies(@Query("api_key") apiKey: String = BuildConfig.API_KEY): SearchResponse
 
     @GET("trending/movie/day")
-    suspend fun getTrendingMovies(@Query("api_key") apiKey: String = BuildConfig.API_KEY): Response<TrendingResponse>
+    suspend fun getTrendingMovies(@Query("api_key") apiKey: String = BuildConfig.API_KEY): TrendingResponse
 
     @GET("movie/{movie_id}/recommendations")
     suspend fun getRecommendedMovies(
             @Path("movie_id") movieId: Int,
             @Query("api_key") apiKey: String = BuildConfig.API_KEY
-    ): Response<SearchResponse>
+    ): SearchResponse
 
 }
